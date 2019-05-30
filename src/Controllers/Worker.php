f52a9407b427dead23e80a992f9d97cf56fbea56
Arash Saffari <arash@saffari.com>
2019-05-30 05:19:48 +0200

    test controller

--- src/Controllers/Worker.php
+++ src/Controllers/Worker.php
@@ -1,6 +1,12 @@
 <?php namespace CodeigniterExt\Queue\Controllers;
 
 use CodeIgniter\Controller;
+use CodeigniterExt\Queue\Queue;
+use CodeigniterExt\Queue\Task;
+use CodeigniterExt\Queue\Persistor\Codeigniter\Codeigniter;
+
+use CodeigniterExt\Queue\Entities\QueueJob;
+use CodeigniterExt\Queue\Models\QueueJobModel;
 use Config\Services;
 
 class Worker extends Controller
@@ -25,6 +31,73 @@ class Worker extends Controller
      */
     public static function Done()
     {
-        echo "Done from Queue";
-    }
+
+		$queue = new Queue();
+
+		$task = new Task;
+
+		for ($i=0; $i < 10; $i++) { 
+			
+			$task
+				->setName('App/Controllers/SendMail')
+				->setData(
+					array(
+						'to'        => 'arash@saffari.com',
+						'from'      => 'qutee@nowhere.tld',
+						'subject'   => 'Hi!',
+						'text'      => 'It\'s your faithful QuTee!'
+					)
+				)
+				->setPriority(Task::PRIORITY_HIGH);
+				// ->setUniqueId('send_mail_email'. $i .'@domain.tld');
+
+			// Queue it
+			$queue->addTask($task);
+
+
+
+			$task
+				->setName('App/Controllers/SendMail1')
+				->setData(
+					array(
+						'to'        => 'arash@saffari.com',
+						'from'      => 'qutee@nowhere.tld',
+						'subject'   => 'Hi!',
+						'text'      => 'It\'s your faithful QuTee!'
+					)
+				)
+				->setPriority(Task::PRIORITY_HIGH);
+				// ->setUniqueId('send_mail_email'. $i .'@domain.tld');
+
+			// Queue it
+			$queue->addTask($task);
+
+			
+		}
+		// Create a task
+		
+		
+
+		}
+		
+
+
+		public static function getTask()
+    {
+
+		$queue = new Queue();
+		$worker = new \CodeigniterExt\Queue\Worker();
+		$worker
+			->setQueue($queue)
+			->setInterval(0.5);
+		$task = $worker->getTask();
+		if ($task !== null) {
+			$worker->run($task);
+		}
+		
+
+		echo "Done!";
+
+		
+		}
 }
